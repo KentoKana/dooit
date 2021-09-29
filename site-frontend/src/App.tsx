3be84a0d58cb6c068a65ceb0b2e371eae58045f7
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-09-28 19:28:03 -0700

    Remove unused files.

--- site-frontend/src/App.tsx
+++ site-frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { PrivateRoute } from "./components/PrivateRoute";
