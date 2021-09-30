4cf8d34fdc4aa0af663fa0cf3a348bbf09057c1c
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-09-29 18:40:26 -0700

    Configure TypeORM in NestJs.

--- site-backend/src/app.module.ts
+++ site-backend/src/app.module.ts
@@ -6,10 +6,14 @@ import { AuthStrategy } from './auth/auth.strategy';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController, UserController],
   providers: [AppService, AuthStrategy, UserService],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
