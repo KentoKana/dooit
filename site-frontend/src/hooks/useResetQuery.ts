26681b628d03167e76eb51ff11fe137b0ed9bcf0
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-09-30 17:16:45 -0700

    Modify useResetQuery to reset userToken.

--- site-frontend/src/hooks/useResetQuery.ts
+++ site-frontend/src/hooks/useResetQuery.ts
@@ -1,8 +1,12 @@
 import { useQueryClient } from "react-query";
+import { UseStores } from "../stores/StoreContexts";
 
 export const useResetQuery = () => {
     const queryClient = useQueryClient();
+    const { userStore } = UseStores();
     const reset = () => {
+        userStore.userToken = null;
+        localStorage.removeItem('user-jwt');
         queryClient.setQueryData("dashboard", () => null);
         queryClient.removeQueries("dashboard");
     }
