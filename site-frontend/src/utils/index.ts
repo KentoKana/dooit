10cdfeb9b99d63d3b59e4ca0916d301743f348cd
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-01 20:16:44 -0700

    Add firebase error message handler function.

--- site-frontend/src/utils/index.ts
+++ site-frontend/src/utils/index.ts
@@ -1,3 +1,16 @@
+export const generateFirebaseAuthErrorMessage = (errorCode: string) => {
+    switch (errorCode) {
+        case "auth/user-not-found":
+            return "No user found with matching credentials."
+        case "auth/wrong-password":
+            return "Invalid password."
+        case "auth/too-many-requests":
+            return "Too many wrong attemps. Please try again later."
+        default:
+            return "Something went wrong ..."
+    }
+}
+
 export const isNullOrUndefined = (value: any) => {
     if (value !== null && value !== undefined) {
         return false
