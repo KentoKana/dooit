b8d8fa4452161e0eeaa00a69a94cc33657f59941
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-04 22:16:21 -0700

    Handle route generation using routes file.

--- site-frontend/src/App.tsx
+++ site-frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { PrivateRoute } from "./components/PrivateRoute";
-import { Login } from "./pages/Login";
-import { SignUp } from "./pages/SignUp";
 import { useEffect, useState } from "react";
 import { UseStores } from "./stores/StoreContexts";
 import { AppInit } from "./classes/AppInit";
-import { ForgotPassword } from "./pages/ForgotPassword";
 import { NavBar } from "./components/Layouts/NavBar";
 import { BasePage } from "./components/Layouts/BasePage";
-import { Dashboard } from "./pages/Dashboard";
 import { LocalRoutes } from "./enums/LocalRoutes";
-import { Home } from "./pages/Home";
+import { routes } from "./routes";
 
 export const App = observer(() => {
   const { userStore, uiStore } = UseStores();
@@ -29,21 +25,20 @@ export const App = observer(() => {
           <NavBar />
           <BasePage>
             <Switch>
-              <PrivateRoute
-                exact
-                path={LocalRoutes.Dashboard}
-                isAuthenticated={false}
-                authenticationPath={LocalRoutes.Login}
-                component={Dashboard}
-              />
-              <Route exact path={LocalRoutes.Home} component={Home} />
-              <Route exact path={LocalRoutes.Login} component={Login} />
-              <Route exact path={LocalRoutes.SignUp} component={SignUp} />
-              <Route
-                exact
-                path={LocalRoutes.ForgotPassword}
-                component={ForgotPassword}
-              />
+              {routes.map((route) => {
+                return route.isPrivate ? (
+                  <PrivateRoute
+                    exact
+                    path={route.path}
+                    authenticationPath={
+                      route.authenticationPath ?? LocalRoutes.Login
+                    }
+                    component={route.component}
+                  />
+                ) : (
+                  <Route exact path={route.path} component={route.component} />
+                );
+              })}
             </Switch>
           </BasePage>
         </Router>
