0ca8a185fc71111404db1edf06ca5e073d197ee9
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-05 23:48:32 -0700

    Wrap Dashboard page with PageWrapper.

--- site-frontend/src/pages/Dashboard.tsx
+++ site-frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
+import { observer } from "mobx-react-lite";
 import { Jumbotron } from "../components/Dashboard/Jumbotron";
+import { PageWrapper } from "./PageWrapper";
 
-export const Dashboard = () => {
+export const Dashboard = observer(() => {
   return (
-    <>
+    <PageWrapper>
       <Jumbotron />
-    </>
+    </PageWrapper>
   );
-};
+});
