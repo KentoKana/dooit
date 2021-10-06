9e517f7e8b50b6191b3bfa771bf7f66ab0b0a7c0
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-05 22:36:31 -0700

    Render sidebar links with styling.

--- site-frontend/src/components/Layouts/Sidebar.tsx
+++ site-frontend/src/components/Layouts/Sidebar.tsx
@@ -1,21 +1,45 @@
-import { Box, ListItem, UnorderedList } from "@chakra-ui/layout";
-import { Text } from "@chakra-ui/react";
+import { Box, ListItem, UnorderedList, Text } from "@chakra-ui/layout";
+import { Link } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link as RouteLink } from "react-router-dom";
 import { LocalRoutes } from "../../enums/LocalRoutes";
+import { dashboardRoutes } from "../../routes/dashboardRoutes";
+import { useLocation } from "react-router-dom";
 
 export const Sidebar = observer(() => {
+  const [selectedLink, setSelectedLink] = useState<LocalRoutes>(
+    LocalRoutes.Dashboard
+  );
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentPathname = location.pathname;
+    setSelectedLink(currentPathname as LocalRoutes);
+  }, [location]);
+
   return (
     <Box>
       <Box as="nav" borderRadius={5}>
         <UnorderedList listStyleType="none">
-          <ListItem fontSize="18px" fontWeight="600">
-            <Text color="primary">
-              <Link to={LocalRoutes.Dashboard}>Dashboard</Link>
-            </Text>
-          </ListItem>
-          <ListItem>Hi</ListItem>
-          <ListItem>Hi</ListItem>
+          {dashboardRoutes.map((route) => {
+            return (
+              <ListItem fontWeight="600" key={route.url}>
+                <Link
+                  onClick={() => {}}
+                  as={RouteLink}
+                  to={route.url}
+                  variant={
+                    route.url === selectedLink
+                      ? "dashboard_active"
+                      : "dashboard_inactive"
+                  }
+                >
+                  <Text ml={5}>{route.label}</Text>
+                </Link>
+              </ListItem>
+            );
+          })}
         </UnorderedList>
       </Box>
     </Box>
---
+++ site-frontend/src/routes/dashboardRoutes.tsx
@@ -0,0 +1,6 @@
+import { LocalRoutes } from "../enums/LocalRoutes";
+
+export const dashboardRoutes = [
+  { url: LocalRoutes.Dashboard, label: "Dashboard" },
+  { url: LocalRoutes.UserSettings, label: "User Setting" },
+];
