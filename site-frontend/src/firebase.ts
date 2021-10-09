d3870e674354a1bbcec6558c56e6a2382cfea052
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-08 20:33:14 -0700

    Add firebase service package to frontend again.

---
+++ site-frontend/src/firebase.ts
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app"
+import { getAuth } from "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN_KEY,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
+const auth = getAuth(app)
+
+export { app, auth }
