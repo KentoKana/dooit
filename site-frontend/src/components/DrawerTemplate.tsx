d7442235c08b2d2a0f0a0afc083a0b0de4b3767b
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-09 22:56:22 -0700

    Create DrawerTemplate component.

---
+++ site-frontend/src/components/DrawerTemplate.tsx
@@ -0,0 +1,50 @@
+import {
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+} from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
+import { ReactNode } from "react";
+
+interface IDrawerTemplateProps {
+  drawerHeader?: ReactNode;
+  drawerFooter?: ReactNode;
+  children?: ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  placement: "right" | "top" | "bottom" | "left";
+  size: "xs" | "sm" | "md" | "lg" | "xl" | "full";
+}
+
+export const DrawerTemplate = observer(
+  ({
+    size = "lg",
+    placement = "left",
+    children,
+    drawerFooter,
+    drawerHeader,
+    isOpen,
+    onClose,
+  }: IDrawerTemplateProps) => {
+    return (
+      <Drawer
+        isOpen={isOpen}
+        placement={placement}
+        onClose={onClose}
+        size={size}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          {drawerHeader && <DrawerHeader>{drawerHeader}</DrawerHeader>}
+          {children && <DrawerBody>{children}</DrawerBody>}
+          {drawerFooter && <DrawerFooter>{drawerFooter}</DrawerFooter>}
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+);
