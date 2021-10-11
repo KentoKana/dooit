0e9f50f8f53611e5a4f2cd6266307e0c97c518bc
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-10-10 21:51:14 -0700

    Attach a history listener on NavBar to set new jwt token if any (experimenting to see if this method will be optimal...).

--- site-frontend/src/components/Layouts/NavBar.tsx
+++ site-frontend/src/components/Layouts/NavBar.tsx
@@ -10,21 +10,36 @@ import {
 } from "@chakra-ui/react";
 import { UnlockIcon } from "@chakra-ui/icons";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UseStores } from "../../stores/StoreContexts";
 import { useEffect, useState } from "react";
 import { LocalRoutes } from "../../enums/LocalRoutes";
 import { AiOutlinePlus } from "react-icons/ai";
 import { CreationDrawer } from "../CreationDrawer";
+import { auth } from "../../firebase";
 
 export const NavBar = observer(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
 
   const { userStore } = UseStores();
   const [isSignedIn, setIsSignedIn] = useState(false);
   useEffect(() => {
     setIsSignedIn(userStore.isSignedIn);
   }, [userStore.isSignedIn]);
+
+  useEffect(() => {
+    history?.listen(() => {
+      auth.onAuthStateChanged(async (user) => {
+        const newToken = await user?.getIdToken();
+        if (newToken) {
+          localStorage.setItem("user-jwt", newToken);
+          userStore.userToken = newToken;
+        }
+      });
+    });
+  }, [history, userStore]);
+
   return (
     <Box position="fixed" width="100%" backgroundColor={"white"} top="0">
       <Container maxW="container.xl" height="80px">
