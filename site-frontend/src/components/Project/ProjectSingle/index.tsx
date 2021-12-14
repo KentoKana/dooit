963ef55412126137b5b0e5fb1746294f7ef2a2df
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-12-13 20:00:23 -0800

    Update swiper carousel styling.

--- site-frontend/src/components/Project/ProjectSingle/index.tsx
+++ site-frontend/src/components/Project/ProjectSingle/index.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useGetProjectById } from "../../../hooks/data/useGetProjectById";
-import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image, Button } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react/";
 import SwiperCore, { Keyboard, Navigation, Pagination } from "swiper";
 
@@ -8,11 +8,16 @@ import "swiper/swiper.scss"; // core Swiper
 import "swiper/components/navigation/navigation.scss"; // Navigation module
 import "swiper/components/scrollbar/scrollbar.scss"; // Navigation module
 import "swiper/components/pagination/pagination.scss"; // Navigation module
+import { useRef } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import "./style.css";
 
 SwiperCore.use([Keyboard, Navigation, Pagination]);
 
 export const ProjectSingle = () => {
   const { projectId } = useParams<{ username: string; projectId: string }>();
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   const project = useGetProjectById(parseInt(projectId));
   const { data } = project;
@@ -25,15 +30,26 @@ export const ProjectSingle = () => {
         </Box>
         <Box width="70%" p={5} pt={0} display="flex" justifyContent="center">
           <Box mt={7} maxW="600px">
+            <Flex justifyContent="end" gap={3}>
+              <Button ref={prevRef} variant="unstyled">
+                <ChevronLeftIcon fontSize="30px" />
+              </Button>
+              <Button ref={nextRef} variant="unstyled">
+                <ChevronRightIcon fontSize="30px" />
+              </Button>
+            </Flex>
             <Swiper
-              initialSlide={2}
+              initialSlide={0}
               pagination={{ type: "progressbar" }}
               slidesPerView={1}
               spaceBetween={30}
               keyboard={{
                 enabled: true,
               }}
-              navigation={true}
+              navigation={{
+                prevEl: prevRef.current,
+                nextEl: nextRef.current,
+              }}
             >
               {data?.projectItems &&
                 data?.projectItems[0].imageUrl &&
