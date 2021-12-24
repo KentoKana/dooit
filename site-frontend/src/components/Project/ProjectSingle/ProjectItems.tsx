21d7fb8dcb9f497f592842df52b7cabe010029f1
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-12-23 20:31:55 -0800

    Create separate ProjectItems component for single project view.

---
+++ site-frontend/src/components/Project/ProjectSingle/ProjectItems.tsx
@@ -0,0 +1,67 @@
+import { useRef } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Box, Flex, Text, Image, Button } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react/";
+import SwiperCore, {
+  Keyboard,
+  Navigation,
+  Pagination,
+  Mousewheel,
+} from "swiper";
+
+import "swiper/swiper.scss"; // core Swiper
+import "swiper/components/navigation/navigation.scss"; // Navigation module
+import "swiper/components/scrollbar/scrollbar.scss"; // Navigation module
+import "swiper/components/pagination/pagination.scss"; // Navigation module
+
+import "./style.css";
+import { ProjectGetOneDto } from "../../../Dtos/project/ProjectGetOneDto";
+
+SwiperCore.use([Keyboard, Navigation, Pagination, Mousewheel]);
+interface IProjectItemsProps {
+  data?: ProjectGetOneDto;
+}
+export const ProjectItems = ({ data }: IProjectItemsProps) => {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  return (
+    <Box mt={7} maxW="600px">
+      <Flex justifyContent="end" gap={3}>
+        <Button ref={prevRef} variant="unstyled">
+          <ChevronLeftIcon fontSize="30px" />
+        </Button>
+        <Button ref={nextRef} variant="unstyled">
+          <ChevronRightIcon fontSize="30px" />
+        </Button>
+      </Flex>
+      <Swiper
+        mousewheel
+        initialSlide={0}
+        pagination={{ type: "progressbar" }}
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+      >
+        {data?.projectItems &&
+          data?.projectItems[0].imageUrl &&
+          data?.projectItems[0].description &&
+          data?.projectItems?.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <Image src={item.imageUrl} alt={item.description} w="100%" />
+                <Box mt={5}>
+                  <Text>{item.description}</Text>
+                </Box>
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </Box>
+  );
+};
--- site-frontend/src/components/Project/ProjectSingle/index.tsx
+++ site-frontend/src/components/Project/ProjectSingle/index.tsx
@@ -1,23 +1,11 @@
 import { useParams } from "react-router";
 import { useGetProjectById } from "../../../hooks/data/useGetProjectById";
-import { Box, Flex, Heading, Text, Image, Button } from "@chakra-ui/react";
-import { Swiper, SwiperSlide } from "swiper/react/";
-import SwiperCore, { Keyboard, Navigation, Pagination } from "swiper";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
-import "swiper/swiper.scss"; // core Swiper
-import "swiper/components/navigation/navigation.scss"; // Navigation module
-import "swiper/components/scrollbar/scrollbar.scss"; // Navigation module
-import "swiper/components/pagination/pagination.scss"; // Navigation module
-import { useRef } from "react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import "./style.css";
-
-SwiperCore.use([Keyboard, Navigation, Pagination]);
+import { ProjectItems } from "./ProjectItems";
 
 export const ProjectSingle = () => {
   const { projectId } = useParams<{ username: string; projectId: string }>();
-  const prevRef = useRef(null);
-  const nextRef = useRef(null);
 
   const project = useGetProjectById(parseInt(projectId));
   const { data } = project;
@@ -29,47 +17,7 @@ export const ProjectSingle = () => {
           <Text>{data?.description}</Text>
         </Box>
         <Box width="70%" p={5} pt={0} display="flex" justifyContent="center">
-          <Box mt={7} maxW="600px">
-            <Flex justifyContent="end" gap={3}>
-              <Button ref={prevRef} variant="unstyled">
-                <ChevronLeftIcon fontSize="30px" />
-              </Button>
-              <Button ref={nextRef} variant="unstyled">
-                <ChevronRightIcon fontSize="30px" />
-              </Button>
-            </Flex>
-            <Swiper
-              initialSlide={0}
-              pagination={{ type: "progressbar" }}
-              slidesPerView={1}
-              spaceBetween={30}
-              keyboard={{
-                enabled: true,
-              }}
-              navigation={{
-                prevEl: prevRef.current,
-                nextEl: nextRef.current,
-              }}
-            >
-              {data?.projectItems &&
-                data?.projectItems[0].imageUrl &&
-                data?.projectItems[0].description &&
-                data?.projectItems?.map((item) => {
-                  return (
-                    <SwiperSlide key={item.id}>
-                      <Image
-                        src={item.imageUrl}
-                        alt={item.description}
-                        w="100%"
-                      />
-                      <Box mt={5}>
-                        <Text>{item.description}</Text>
-                      </Box>
-                    </SwiperSlide>
-                  );
-                })}
-            </Swiper>
-          </Box>
+          <ProjectItems data={data} />
         </Box>
       </Flex>
     </Box>
